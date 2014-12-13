1b1487e87eba2819c195091259eca9159199f71b
Brayden Lopez <blopez@instructure.com>
2014-12-12 18:20:45 -0700

    Not finished. But its progress!

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,39 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var rename = require('gulp-rename');
-var concat = require('gulp-concat');
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var catify      = require('gulp-concat');
+var uglyfy      = require('gulp-uglify');
+var mincss      = require('gulp-minify-css');
 
-var source = require('vinyl-source-stream')
-var browserify = require('browserify');
-var partialify = require('partialify');
-
-var jsPath = './app/**/*';
-var cssPath = './app/components/**/*.scss';
-
-gulp.task('browserify', function() {
-  var bundleStream = browserify('./app/main.js')
-    .transform(partialify)
-    .bundle();
-
-  bundleStream
-    .pipe(source('main.js'))
-    .pipe(gulp.dest('./public/js/'));
-});
-
-gulp.task('css', function() {
-  gulp.src(cssPath)
-    .pipe(concat('main.css'))
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }))
+gulp.task('sass', function() {
+   gulp.src('./app/scss/main.scss')
+    .pipe(catify('main.css'))
+    .pipe(sass())
     .pipe(gulp.dest('./public/css/'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch(jsPath, ['browserify']);
-  gulp.watch(cssPath, ['css']);
+gulp.task('css', function() {
+   gulp.src('./app/scss/vendor/*.css')
+    .pipe(catify('vendor.css'))
+    .pipe(mincss({keepSpecialComments: 0}))
+    .pipe(gulp.dest('./public/css/'));
 });
 
-gulp.task('default', ['css', 'browserify', 'watch']);
+gulp.task('js', function() {
+  gulp.src('./app/js/*.js')
+    .pipe(catify('main.js'))
+    .pipe(uglyfy())
+    .pipe(gulp.dest('./public/js/'))
+})
+
+gulp.task('vendorjs', function() {
+  gulp.src('./app/js/vendor/*.js')
+    .pipe(uglyfy())
+    .pipe(gulp.dest('./public/js/vendor/'))
+})
+
+gulp.task('watch', function() {
+   //gulp.watch('./app/scss/*.scss', ['sass']);
+});
+
+ 
+ gulp.task('default', ['sass', 'js', 'vendorjs','css', 'watch']);
